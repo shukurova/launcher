769c2c161ba224e94385676cf775df7aa502861d
ALINA_SHUKUROVA <alina.shukurova@simbirsoft.com>
2019-06-26 16:38:12 +0300

    Add authorities table

--- schema.sql
+++ schema.sql
@@ -1,12 +1,18 @@
 CREATE TABLE users
 (
     id                      SERIAL PRIMARY KEY,
-    name                    TEXT         NOT NULL,
-    username                VARCHAR(255) NOT NULL UNIQUE,
-    password                VARCHAR(20)  NOT NULL,
-    authorities             TEXT[]       NOT NULL DEFAULT ARRAY[]::TEXT[],
-    account_non_expired     BOOLEAN               DEFAULT TRUE,
-    account_non_locked      BOOLEAN               DEFAULT TRUE,
-    credentials_non_expired BOOLEAN               DEFAULT TRUE,
-    enabled                 BOOLEAN               DEFAULT FALSE
+    name                    TEXT NOT NULL,
+    username                TEXT NOT NULL UNIQUE,
+    password                TEXT NOT NULL,
+    authority               INTEGER NOT NULL REFERENCES authorities(role_id),
+    account_non_expired     BOOLEAN       DEFAULT TRUE,
+    account_non_locked      BOOLEAN       DEFAULT TRUE,
+    credentials_non_expired BOOLEAN       DEFAULT TRUE,
+    enabled                 BOOLEAN       DEFAULT FALSE
+);
+
+CREATE TABLE authorities (
+    id SERIAL,
+    role_id INTEGER NOT NULL PRIMARY KEY,
+    role TEXT NOT NULL DEFAULT 'ROLE_USER'
 );
