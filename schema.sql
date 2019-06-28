f4a63754aabd90d4345874ba498f62fddf048d63
ALINA_SHUKUROVA <alina.shukurova@simbirsoft.com>
2019-06-28 17:15:45 +0300

    Data base structure

--- schema.sql
+++ schema.sql
@@ -4,15 +4,56 @@ CREATE TABLE users
     name                    TEXT NOT NULL,
     username                TEXT NOT NULL UNIQUE,
     password                TEXT NOT NULL,
-    authority               INTEGER NOT NULL REFERENCES authorities(role_id),
-    account_non_expired     BOOLEAN       DEFAULT TRUE,
-    account_non_locked      BOOLEAN       DEFAULT TRUE,
-    credentials_non_expired BOOLEAN       DEFAULT TRUE,
-    enabled                 BOOLEAN       DEFAULT FALSE
+    account_non_expired     BOOLEAN DEFAULT TRUE,
+    account_non_locked      BOOLEAN DEFAULT TRUE,
+    credentials_non_expired BOOLEAN DEFAULT TRUE,
+    enabled                 BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE authorities (
-    id SERIAL,
-    role_id INTEGER NOT NULL PRIMARY KEY,
-    role TEXT NOT NULL DEFAULT 'ROLE_USER'
+CREATE TABLE authorities
+(
+    id        SERIAL PRIMARY KEY,
+    user_id   INTEGER NOT NULL REFERENCES users,
+    authority TEXT    NOT NULL DEFAULT 'ROLE_USER' -- ROLE_ADMIN, ROLE_DEVELOPER, ROLE_APPROVER
+);
+
+CREATE TABLE games
+(
+    id         SERIAL PRIMARY KEY,
+    name       TEXT    NOT NULL UNIQUE,
+    content    TEXT    NOT NULL,
+    coverage   TEXT    NOT NULL,
+    company_id INTEGER NOT NULL REFERENCES companies,
+    status     TEXT    NOT NULL DEFAULT 'TODO', -- 'IN_PROGRESS', 'BETA', 'RELEASE'
+    genre      TEXT    NOT NULL,
+    likes      INTEGER          DEFAULT 0,
+    dislikes   INTEGER          DEFAULT 0
+);
+
+CREATE TABLE comments
+(
+    id       SERIAL PRIMARY KEY,
+    user_id  INTEGER NOT NULL REFERENCES users,
+    game_id  INTEGER NOT NULL REFERENCES games,
+    content  TEXT    NOT NULL,
+    date     DATE    DEFAULT CURRENT_DATE,
+    likes    INTEGER DEFAULT 0,
+    dislikes INTEGER DEFAULT 0
+);
+
+CREATE TABLE companies
+(
+    id            SERIAL PRIMARY KEY,
+    name          TEXT NOT NULL UNIQUE,
+    country       TEXT NOT NULL,
+    content       TEXT NOT NULL,
+    date_creation DATE NOT NULL
+);
+
+CREATE TABLE medias
+(
+    id      SERIAL PRIMARY KEY,
+    game_id INTEGER NOT NULL REFERENCES games,
+    path    TEXT    NOT NULL,
+    type    TEXT    NOT NULL -- 'IMAGE', 'VIDEO'
 );
